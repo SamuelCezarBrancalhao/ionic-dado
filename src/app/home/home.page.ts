6811cde73bc0a4093c51c9997f41f79ddd494831
SamuelCezarBrancalhao <samuelcezarbrancalhao@gmail.com>
2022-08-16 20:17:55 -0300

    Projeto Finalizado

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  dado = 0;
+  constructor() {}
+  async sortear() {
+    for(let i = 0; i <10; i++) {
+  let numeroSorteado = Math.random();
+  this.dado = Math.floor(numeroSorteado * 6 + 1)
+  console.log(this.dado);
+  await new Promise(f => setTimeout(f, 100));
+    }
+  }
+}
